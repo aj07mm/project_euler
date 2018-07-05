1fe38d88377412d6d3adba5b358b8cac702f8c3c
aj07mm <aj07mm@hotmail.com>
2018-07-04 23:03:06 -0300

    problem 10 - summation of primes using sieve of atkin

--- parallel.py
+++ parallel.py
@@ -1,14 +1,43 @@
-import multiprocessing
-import numpy as np
+# https://projecteuler.net/problem=10
+import math
+import unittest
+from multiprocessing import Pool, Process
 
-data_pairs = [ [3,5], [4,3], [7,3], [1,6] ]
+def is_prime(m):
+    acc = 0
+    for n in range(1, m+1):
+        acc += 1 if m%n == 0 else 0
+        if acc > 2:
+            return False
+    return True
+        
+def get_primes(nth_start, nth_end):
+    primes = []
+    for n in range(nth_start, nth_end+1):
+        if is_prime(n):
+            primes.append(n)
+            print(len(primes))
+    return primes
+        
 
-# define what to do with each data pair ( p=[3,5] ), example: calculate product 
-def myfunc(p):
-    product_of_list = np.prod(p)
-    return product_of_list
+def summation_of_primes(nth_start, nth_end):
+    print("starting: ", nth_start, nth_end)
+    return sum(get_primes(nth_start, nth_end))
+
+
+def pool_fn(fn):
+    p = Pool(5)
+    res = p.starmap(fn, [(2, 1000000), (1000000, 2000000)])
+    print("finish")
+    print(res)
+    return sum(res)
+
+
+class TestSmallestMultiple(unittest.TestCase):
+
+    def test_1_1(self):
+        self.assertEqual(pool_fn(summation_of_primes), 123)
+
 
 if __name__ == '__main__':
-    pool = multiprocessing.Pool(processes=4)
-    result_list = pool.map(myfunc, data_pairs)
-    print(result_list)
+    unittest.main()
--- problem_010.py
+++ problem_010.py
@@ -1,39 +1,47 @@
 # https://projecteuler.net/problem=10
 import math
 import unittest
-from multiprocessing import Pool
-
-def is_prime(m):
-    acc = 0
-    for n in range(1, m+1):
-        acc += 1 if m%n == 0 else 0
-        if acc > 2:
-            return False
-    return True
-        
-def get_primes(nth_start, nth_end):
-    primes = []
-    for n in range(nth_start, nth_end+1):
-        if is_prime(n):
-            primes.append(n)
-            print(len(primes))
-    return primes
-        
-
-def summation_of_primes(nth_start, nth_end):
-    return sum(get_primes(nth_start, nth_end))
+from multiprocessing import Pool, Process
 
 
-def pool_fn(fn):
-    p = Pool(2)
-    res1 = p.map(fn, 2, 1000000)
-    res2 = p.map(fn, 1000000, 2000000)
+def sieve_of_eratosthenes(n):
+    multiples = []
+    for i in range(2, n+1):
+        if i not in multiples:
+            print (i)
+            for j in range(i*i, n+1, i):
+                multiples.append(j)
 
 
-class TestSmallestMultiple(unittest.TestCase):
+def sieve_of_atkin(limit):
+    P = [2,3]
+    sieve=[False]*(limit+1)
+    for x in range(1,int(math.sqrt(limit))+1):
+        for y in range(1,int(math.sqrt(limit))+1):
+            n = 4*x**2 + y**2
+            if n<=limit and (n%12==1 or n%12==5) : sieve[n] = not sieve[n]
+            n = 3*x**2+y**2
+            if n<= limit and n%12==7 : sieve[n] = not sieve[n]
+            n = 3*x**2 - y**2
+            if x>y and n<=limit and n%12==11 : sieve[n] = not sieve[n]
+        print(x, y)
+    for x in range(5,int(math.sqrt(limit))):
+        if sieve[x]:
+            for y in range(x**2,limit+1,x**2):
+                sieve[y] = False
+    for p in range(5,limit):
+        if sieve[p] : P.append(p)
+    return P
+
+
+def summation_of_primes(nth):
+    return sum(sieve_of_atkin(nth))
+
+
+class TestSummationOfPrimes(unittest.TestCase):
 
     def test_1_1(self):
-        self.assertEqual(pool_fn(summation_of_primes), 123)
+        self.assertEqual(summation_of_primes(2000000), 142913828922)
 
 
 if __name__ == '__main__':
